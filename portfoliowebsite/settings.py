1e369f88b36d05a83ad9a9bb8d6523f239897460
Julien Munar <julienmunar@MacBook-Pro-de-Julien.local>
2021-08-01 18:25:08 +0200

    profile

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #added MiddleWare
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfoliowebsite.urls'
